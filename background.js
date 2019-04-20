6533ea0814fbcd10893dcc6af6858f1f0dbc0f72
Joshua Michael <joshsrn@gmail.com>
2019-04-20 08:53:54 -0400

    send current page to ssllabs on click

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    alert('working?');
+    let url =
+        'https://ssllabs.com/ssltest/analyze.html?d=' + tab.url;
+    chrome.tabs.create({url: url, index: tab.index + 1});
 });
